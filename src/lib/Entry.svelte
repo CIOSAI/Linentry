1fc40512d8c953e86c1661e661f15330ab091ed8
CIOSAI <jyinteractive.tw@gmail.com>
2022-11-27 01:33:20 +0800

    scrolling basics

--- src/lib/Entry.svelte
+++ src/lib/Entry.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
   import {type RenderedLine, namesToCommands} from '$lib/linentry'
   import AutocompleteText from './AutocompleteText.svelte';
+  export let height = 40
   export let line:RenderedLine
   export let ind = 0
   export let isRunning = false
   export let isMain = false
   export let onCommandChanged = (index:number, str:string)=>{}
+
+  let lineData = line.data
+  $: for(let i=lineData.length; i<8; i++) lineData.push(NaN)
 </script>
 
-<tr class='entry'>
+<tr class='entry' style='height: {height}px'>
   <td class='index' style='border: {isMain?'2px':'0'} solid #000'>{ind}</td>
   <td class='tick' style='color: {isRunning?'#0f0':'#00000000'}'>{'>'}</td>
   <td class='command'>
@@ -18,8 +22,8 @@
       valueHandler={(str)=>{onCommandChanged(ind, str)}}
     />
   </td>
-  {#each line.data as i}
-    <td class='unit'><input type='text' value={i} maxlength="2"></td>
+  {#each lineData as i}
+    <td class='unit'><input type='text' value={i?i:''} maxlength="2"></td>
   {/each}
 </tr>
 
--- src/lib/SpreadSheet.svelte
+++ src/lib/SpreadSheet.svelte
@@ -6,18 +6,25 @@
   export let renderedLines = linentry.src.line.map(v=>{ return {command: v.command.name, data: v.data} })
   export let currentLine = linentry.src.main
 
+  $: {
+    for(let i=renderedLines.length; i<8; i++){
+      renderedLines.push({command: COMMANDS.NOCOMMAND.name, data: []})
+    }
+  }
+
   let scrollY = 0;
 </script>
 
 <table 
   on:wheel={(e)=>{
   // @ts-ignore
-  scrollY += e.wheelDeltaY
+  scrollY += e.wheelDeltaY/10
   }}
-  style='top: {scrollY/10}px'
+  style='top: {scrollY%30}px'
 >
   {#each renderedLines as i, ind}
-    <Entry line={i} ind={ind} isMain={ind==linentry.src.main} isRunning={ind==currentLine}
+    <Entry height={30} line={i} 
+      ind={ind} isMain={ind==linentry.src.main} isRunning={ind==currentLine}
       onCommandChanged={(index, str)=>{
         // @ts-ignore
         linentry.src.line[index].command = namesToCommands.has(str)?namesToCommands.get(str):COMMANDS.NOCOMMAND
